8461f6ba20335783387f6c0694583720795ad5a5
NAOYUKI UMEDA <naoyuki.umd@hotmail.co.jp>
2023-04-29 13:31:21 +0900

    非同期処理setTimeout関数

--- modernJS.js
+++ modernJS.js
@@ -1,2 +1,8 @@
-//firstCommit
-console.log("firstCommit");
+//非同期処理
+console.log("非同期処理");
+
+const callbackFunc = () => console.log('処理が実行されました');
+
+//500ミリ秒後に1度だけ処理が実行される
+setTimeout(callbackFunc, 500);
+　console.log('callback関数の前に実行される');
