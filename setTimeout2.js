d84484fbd79308de9e0bc2f08c57a8d1d8f90ed6
NAOYUKI UMEDA <naoyuki.umd@hotmail.co.jp>
2023-04-29 13:42:58 +0900

    setTimeout2追加

---
+++ setTimeout2.js
@@ -0,0 +1,13 @@
+const sampleCallbaclFunc = () => {
+    console.log('最初');
+
+//setTimeout関数の第1引数に渡したコールバック関数を0ミリ秒後に実行
+setTimeout( () => console.log('中盤'), 0);
+
+console.log('最後');
+}
+
+//関数の実行
+sampleCallbaclFunc();
+
+//中盤がタイマー処理の実行中に,[最後]が非同期処理の実行
